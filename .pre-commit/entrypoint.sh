ab172fb5d885a95876fafefa6211b512107f5377
carnarez <11967125+carnarez@users.noreply.github.com>
2022-01-22 17:47:01 +0100

    Updated pre-commit

--- .pre-commit/entrypoint.sh
+++ .pre-commit/entrypoint.sh
@@ -9,14 +9,26 @@ declare -A hooks
 hooks=([md]=mdformat [py]=black,flake8,isort,mypy,pydocstyle)
 
 
-# fetch the list of modified files from git itself
 # grep by extension defined in the associative array
+# fetch the list of modified files corresponding to this extension from git itself
 # if hook actions are required increment the error code before making them happen
 # issues with bash variables and nested loops in subshells asked for a lock file
 
 for e in "${!hooks[@]}"; do
-  git diff --name-only --diff-filter=ACM | sort | uniq | grep ".$e$" | while read -r f; do
-    for h in ${hooks[$e]//,/ }; do
+  for h in ${hooks[$e]//,/ }; do
+
+    # list files from this commit; if an index.lock is present we expect it to be due to
+    # modified but manually unstaged files to be part of the commit
+    if [ -f .git/index.lock ]; then
+        s=$(git diff --cached --diff-filter=ACM --name-only | grep -e "\.$e$" | tr "\n" " ")
+        u=$(git diff --diff-filter=ACM --name-only | grep -e "\.$e$" | tr "\n" " ")
+        files="$s $u"
+    else
+        files=$(git commit --short | grep -E "^[ACM]" | grep -e "\.$e$" | awk '{printf" %s",$2}')
+    fi
+
+    # check and apply hook action
+    if ([ ! -z "$files" ] && [ "$files" != " " ]); then
       echo -e "\n$(tput bold)$h:$(tput sgr0)"
 
       obj=$(grep -v "^\s*#" /usr/share/pre-commit/hooks.y*ml | grep --after-context=3 "^$h:")
@@ -24,15 +36,15 @@ for e in "${!hooks[@]}"; do
       flags=$(awk '$1=="flags:" {$1="";print$0}' <<< "$obj")
       check=$(awk '$1=="check:" {$1="";print$0}' <<< "$obj")
 
-      if ! eval "$cmd $check $flags $f" &>/dev/null; then
-        eval "$cmd $flags $f" 2>&1 | sed 's/^/ /g'
+      if ! eval "$cmd $check $flags $files" &>/dev/null; then
+        eval "$cmd $flags $files" 2>&1 | sed 's/^/ /g'
         touch .commitlock
         echo -e "$(tput setab 1)$cmd$flags$(tput sgr0)"
       else
         echo -e "$(tput setab 2)$cmd$flags$(tput sgr0)"
       fi
+    fi
 
-    done
   done
 done
 
