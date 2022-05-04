861e8a5fca4cde62573c733b7915bf94478ba2fb
carnarez <11967125+carnarez@users.noreply.github.com>
2022-05-04 11:50:51 +0200

    Framework in place

---
+++ tests/test_astdocs.py
@@ -0,0 +1,45 @@
+"""All tests related to the `astdocs.astdocs` module."""
+
+
+def test_format_annotation():
+    raise AssertionError
+
+
+def test_format_docstring():
+    raise AssertionError
+
+
+def test_parse_classdef():
+    raise AssertionError
+
+
+def test_parse_functiondef():
+    raise AssertionError
+
+
+def test_parse_import():
+    raise AssertionError
+
+
+def test_parse_tree():
+    raise AssertionError
+
+
+def test_render_classdef():
+    raise AssertionError
+
+
+def test_render_functiondef():
+    raise AssertionError
+
+
+def test_render_module():
+    raise AssertionError
+
+
+def test_render():
+    raise AssertionError
+
+
+def test_render_recursively():
+    raise AssertionError
